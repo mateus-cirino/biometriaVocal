9ecfb97b85049eb260586ff6a262a55413c79dbf
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-08-14 21:11:36 -0300

    adicao de comentarios

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ import librosa
 
 file_extension = ".ogg"
 
-arr = []
+arrOfAudios = []
 
-i = 1
+numberOfAudio = 1
 
-while i < 34:
-    file_path = str(i) + file_extension
+    # numberOfAudio < 34, pois eu estou lendo 33 áudios que se encontram na minha pasta local
+while numberOfAudio < 34:
+    file_path = str(numberOfAudio) + file_extension
+    #VARIAVEIS
+    # sr (caso eu queira mudar a taxa de amostragem)
+    # mono (se o áudio é mono ou stereo)
+    # offset (caso eu queira começar de uma determinada parte do áudio)
+    # duration (caso eu queira pegar somente uma parte do áudio)
     samples, sampling_rate = librosa.load(
-        file_path,
-        sr=None,
-        mono=True,
-        offset=0.0,
-        duration=None
-    )
-    arr.append([samples,sampling_rate])
-    i += 1
-print(len(arr))
+                                file_path,
+                                sr=None,
+                                mono=True,
+                                offset=0.0,
+                                duration=None
+                                )
+    # samples contém um array de amostras
+    # sampling_rate contém a quantidade de amostras capturadas por segundo
+    # logo, podemos concluir que a duração de um áudio é igual a quantidadeDeAmostras/quantidadeDeAmostrasPorSegundo
+    arrOfAudios.append([samples,sampling_rate])
+    numberOfAudio += 1
+print(len(arrOfAudios))
