fd0143b82b2200dab56d2b1d410b90da0d9e5832
mateus-cirino <mateuscirinosilva32@gmail.com>
2020-09-10 21:34:32 -0300

    correcoes

--- main.py
+++ main.py
@@ -1,53 +1,50 @@
-from librosa import display
+from pybrain.tools.customxml import NetworkWriter
+from pybrain.datasets.supervised import SupervisedDataSet
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.supervised.trainers import BackpropTrainer
+
 from librosa import core
+
 from scipy import fft
-from matplotlib import pyplot
 
-import numpy as np
+import matplotlib.pyplot as plt
 
-file_extension = ".ogg"
+extensaoDoArquivo = ".ogg"
 
-arrOfAudios = []
+arrayDeFFTDosAudios = []
 
-numberOfAudio = 1
+numeroDeAudios = 1
 
-    # numberOfAudio < 34, pois eu estou lendo 33 áudios que se encontram na minha pasta local
-while numberOfAudio < 34:
-    file_path = str(numberOfAudio) + file_extension
-    #VARIAVEIS
-    # sr (caso eu queira mudar a taxa de amostragem)
-    # mono (se o áudio é mono ou stereo)
-    # offset (caso eu queira começar de uma determinada parte do áudio)
-    # duration (caso eu queira pegar somente uma parte do áudio)
-    samples, samplingRate = core.load(
-                                file_path,
-                                sr=None,
-                                mono=True,
-                                offset=0.0,
-                                duration=None
-                                )
-    # samples contém um array de amostras
-    # samplingRate contém a quantidade de amostras capturadas por segundo
-    # logo, podemos concluir que a duração de um áudio é igual a quantidadeDeAmostras/quantidadeDeAmostrasPorSegundo
-    arrOfAudios.append([samples,samplingRate])
-    numberOfAudio += 1
+while numeroDeAudios < 16:
+    caminhoDoArquivo = str(numeroDeAudios) + extensaoDoArquivo
+    amostras, numeroDeAmostrasPorSegundo = core.load(caminhoDoArquivo, sr=None, mono=True, offset=0.0, duration=None)
+    arrayDeFFTDosAudios.extend(fft(amostras))
+    numeroDeAudios += 1
 
-samplesOfAudio1 = arrOfAudios[0][0]
-samplesRateOfAudio1 = arrOfAudios[0][1]
+network = buildNetwork(48000, 100, 100, 5)
+dataSet = SupervisedDataSet(48000, 5)
 
-#Exibição do gráfico que representa o sinal do áudio no dominio da amplitude
-pyplot.figure()
-display.waveplot(y = samplesOfAudio1, sr = samplesRateOfAudio1)
-pyplot.xlabel("Time (seconds) --> ")
-pyplot.ylabel("Amplitude")
-pyplot.show()
+while len(arrayDeFFTDosAudios) > 48000:
+    amostrasPorSegundo = arrayDeFFTDosAudios[:48000]
+    del arrayDeFFTDosAudios[:48000]
+    dataSet.addSample(amostrasPorSegundo, (10, 10, 10, 10, 10))
 
-#Exibição do gráfico que representa o sinal do áudio no dominio da frequência
-fftOut = fft(samplesOfAudio1)
-pyplot.figure()
-pyplot.plot(samplesOfAudio1, np.abs(fftOut))
-pyplot.xlabel("Frequence")
-pyplot.ylabel("Magnitude")
-pyplot.show()
+trainer = BackpropTrainer(network, dataSet)
+error = 1
+iteration = 0
+outputs = []
+while error > 0.001:
+    error = trainer.train()
+    outputs.append(error)
+    iteration += 1
+    print(iteration, error)
+
+NetworkWriter.writeToFile(network, 'network.xml')
+
+plt.ioff()
+plt.plot(outputs)
+plt.xlabel('Iterações')
+plt.ylabel('Erro Quadrático')
+plt.show()
 
 
