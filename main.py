3e1fabe993e2e4190b6c562597c89dbd9fa0c97d
mateus-cirino <mateuscirinosilva32@gmail.com>
2020-09-11 16:32:48 -0300

    correcoes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pybrain.tools.customxml import NetworkWriter
+from pybrain.tools.customxml import NetworkWriter, NetworkReader
 from pybrain.datasets.supervised import SupervisedDataSet
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
@@ -7,22 +7,23 @@ from librosa import core
 
 from scipy import fft
 
-import matplotlib.pyplot as plt
+import csv
 
 extensaoDoArquivo = ".ogg"
 
+network = buildNetwork(48000, 100, 100, 5)
+network = NetworkReader.readFrom('network.xml');
+
+dataSet = SupervisedDataSet(48000, 5)
+
+numeroDeAudios = 16
 arrayDeFFTDosAudios = []
 
-numeroDeAudios = 1
-
-while numeroDeAudios < 16:
-    caminhoDoArquivo = str(numeroDeAudios) + extensaoDoArquivo
+while numeroDeAudios < 23:
+    caminhoDoArquivo = "audiosMateusTreinamento/" + str(numeroDeAudios) + extensaoDoArquivo
     amostras, numeroDeAmostrasPorSegundo = core.load(caminhoDoArquivo, sr=None, mono=True, offset=0.0, duration=None)
     arrayDeFFTDosAudios.extend(fft(amostras))
     numeroDeAudios += 1
-
-network = buildNetwork(48000, 100, 100, 5)
-dataSet = SupervisedDataSet(48000, 5)
 
 while len(arrayDeFFTDosAudios) > 48000:
     amostrasPorSegundo = arrayDeFFTDosAudios[:48000]
@@ -30,21 +31,38 @@ while len(arrayDeFFTDosAudios) > 48000:
     dataSet.addSample(amostrasPorSegundo, (10, 10, 10, 10, 10))
 
 trainer = BackpropTrainer(network, dataSet)
-error = 1
 iteration = 0
-outputs = []
+error = 1
 while error > 0.001:
     error = trainer.train()
-    outputs.append(error)
-    iteration += 1
     print(iteration, error)
+    iteration += 1
 
 NetworkWriter.writeToFile(network, 'network.xml')
 
-plt.ioff()
-plt.plot(outputs)
-plt.xlabel('Iterações')
-plt.ylabel('Erro Quadrático')
-plt.show()
+numeroDeAudios = 1
+arrayDeFFTDosAudios = [];
 
+while numeroDeAudios < 4:
+    caminhoDoArquivo = "audiosMateusValidacao/" + str(numeroDeAudios) + extensaoDoArquivo
+    amostras, numeroDeAmostrasPorSegundo = core.load(caminhoDoArquivo, sr=None, mono=True, offset=0.0, duration=None)
+    arrayDeFFTDosAudios.extend(fft(amostras))
+    numeroDeAudios += 1
 
+with open('treinamentoRedeNeural.csv', mode='w', newline='') as csv_file:
+    nomeDosCampos = ["Nome do arquivo", "Valor esperado", "Valor obtido", "Erro"]
+    writer = csv.DictWriter(csv_file, fieldnames=nomeDosCampos)
+
+    writer.writeheader()
+    numeroDeAudios = 1
+    while len(arrayDeFFTDosAudios) > 48000:
+        amostrasPorSegundo = arrayDeFFTDosAudios[:48000]
+        del arrayDeFFTDosAudios[:48000]
+        saidaDaRedeNeural = network.activate(amostrasPorSegundo)
+        writer.writerow({"Nome do arquivo": (str(numeroDeAudios) + ".ogg"), "Valor esperado": 10, "Valor obtido": saidaDaRedeNeural[0], "Erro": (saidaDaRedeNeural[0] - 10)})
+        writer.writerow({"Nome do arquivo": (str(numeroDeAudios) + ".ogg"), "Valor esperado": 10, "Valor obtido": saidaDaRedeNeural[1], "Erro": (saidaDaRedeNeural[1] - 10)})
+        writer.writerow({"Nome do arquivo": (str(numeroDeAudios) + ".ogg"), "Valor esperado": 10, "Valor obtido": saidaDaRedeNeural[2], "Erro": (saidaDaRedeNeural[2] - 10)})
+        writer.writerow({"Nome do arquivo": (str(numeroDeAudios) + ".ogg"), "Valor esperado": 10, "Valor obtido": saidaDaRedeNeural[3], "Erro": (saidaDaRedeNeural[3] - 10)})
+        writer.writerow({"Nome do arquivo": (str(numeroDeAudios) + ".ogg"), "Valor esperado": 10, "Valor obtido": saidaDaRedeNeural[4], "Erro": (saidaDaRedeNeural[4] - 10)})
+        writer.writerow({"Nome do arquivo": "", "Valor esperado": "", "Valor obtido": "", "Erro": ""})
+        numeroDeAudios += 1
